de9ce47e027c4a288b416faa4cd8e7423170cd75
Roderic Morris <roderic@ccs.neu.edu>
2009-04-28 01:31:49 -0400

    Fix for condition-type/generalizations and condition-type/error?.

--- condition-type.scm
+++ condition-type.scm
@@ -1,11 +1,11 @@
 (define-record-type condition-type
   (make-condition-type name generalization field-names reporter)
   condition-type?
-  (name            condition-type/name)
-  (generalization  condition-type/generalizations)
-  (field-names     condition-type/field-names)
-  (reporter        condition-type/reporter))
+  (name             condition-type/name)
+  (generalizations  condition-type/generalizations)
+  (field-names      condition-type/field-names)
+  (reporter         condition-type/reporter))
 
 (define (condition-type/error? condition)
-  (eq? condition-type:error
-       (condition-type/generalizations condition)))
+  (member condition-type:error
+          (condition-type/generalizations condition)))
