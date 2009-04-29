21a7c87c1efcdc33add2c480b69593202106fe1d
Roderic Morris <roderic@ccs.neu.edu>
2009-04-29 11:06:04 -0400

    Add srfi-89, condition-definition, and restarts structures.

--- packages.scm
+++ packages.scm
@@ -1,11 +1,32 @@
 ;;; -*- Mode: Scheme; scheme48-package: (config) -*-
 
 (define-structure
+    srfi-89
+    srfi-89/interface
+  (open scheme srfi-1 let-opt)
+  (for-syntax (open scheme let-opt (subset signals (syntax-error)) srfi-1))
+  (files srfi-89))
+
+(define-structure
     pantene:condition-type-definition
     pantene:condition-type-definition/interface
   (open scheme srfi-9)
   (files condition-type))
 
+(define-structure
+    pantene:condition-definition
+    pantene:condition-definition/interface
+  (open scheme srfi-1 srfi-9)
+  (files condition))
+
+(define-structure
+    pantene:restarts
+    pantene:restarts/interface
+  (open scheme srfi-1 srfi-9 srfi-39 srfi-89 signals)
+  (files restart))
+
 (define-structure pantene pantene/interface
   (open scheme
-        pantene:condition-type-definition))
+        pantene:condition-type-definition
+        pantene:condition-definition
+        pantene:restarts))
