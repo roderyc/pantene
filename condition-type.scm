ef72cfbc339de794253d50824c356592d7fc18b3
Roderic Morris <roderic@ccs.neu.edu>
2009-05-05 23:33:12 -0400

    Fix make-condition-type.

--- condition-type.scm
+++ condition-type.scm
@@ -1,10 +1,23 @@
 (define-record-type condition-type
-  (make-condition-type name generalizations field-names reporter)
+  (%make-condition-type name generalizations field-names reporter)
   condition-type?
   (name             condition-type/name)
-  (generalizations  condition-type/generalizations)
+  (generalizations  condition-type/generalizations
+                    condition-type/set-generalizations)
   (field-names      condition-type/field-names)
   (reporter         condition-type/reporter))
+
+(define (make-condition-type name generalization field-names reporter)
+  (let* ((condition-type (%make-condition-type name
+                                               #f
+                                               field-names
+                                               reporter))
+         (generalizations (cons condition-type
+                                (if generalization
+                                    (condition-type/generalizations generalization)
+                                    '()))))
+    (condition-type/set-generalizations condition-type generalizations)
+    condition-type))
 
 (define (condition-type/error? condition)
   (member condition-type:error
