3a69d904bf6aebba9235214511a285e09bdf687d
Roderic Morris <roderic@ccs.neu.edu>
2009-06-18 18:20:24 -0400

    Rewrite a restart test using RESTART-CASE.

--- tests/test-restarts.scm
+++ tests/test-restarts.scm
@@ -17,25 +17,17 @@
 ;;; wrong-type-datum conditions in frob-alist that invokes this restart
 ;;; with the value -1. frob-alist-element will signal a wrong-type-datum
 ;;; condition if its argument is not a pair, and so return a -1.
-(letrec ((frob-alist (lambda (alist)
-                       (bind-condition-handler (list condition-type:wrong-type-datum)
-                                               (lambda (condition) (use-value -1 condition))
-                                               (lambda () (map frob-alist-element alist)))))
-         (with-use-value-restart
-          (lambda (thunk)
-            (call-with-current-continuation
-             (lambda (k)
-               (with-restart 'use-value
-                             "Return the given value for the current computatoin"
-                             k
-                             values
-                             thunk)))))
-         (frob-alist-element (lambda (element)
-                               (with-use-value-restart
-                                (lambda ()
-                                  (if (pair? element)
-                                      (+ (cdr element) 10)
-                                      (error condition-type:wrong-type-datum
-                                             (cons 'datum element)
-                                             (cons 'type 'pair))))))))
+(letrec ((frob-alist
+          (lambda (alist)
+            (bind-condition-handler (list condition-type:wrong-type-datum)
+                                    (lambda (condition) (use-value -1 condition))
+                                    (lambda () (map frob-alist-element alist)))))
+         (frob-alist-element
+          (lambda (element)
+            (restart-case (if (pair? element)
+                              (+ (cdr element) 10)
+                              (error condition-type:wrong-type-datum
+                                     (cons 'datum element)
+                                     (cons 'type 'pair)))
+                          ('use-value "Use the given value." (value) value)))))
   (check (frob-alist '((bleh . 23) foo (bar . 4))) => '(33 -1 14)))
