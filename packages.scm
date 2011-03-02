e2f835781cfbeecee45e96f503eb913a6578006e
Roderic Morris <roderyc@gmail.com>
2011-03-02 16:28:52 -0500

    Fix module definitions to avoid renaming warnings.

--- packages.scm
+++ packages.scm
@@ -1,41 +1,30 @@
 ;;; -*- Mode: Scheme; scheme48-package: (config) -*-
 
-(define-structure srfi-8 srfi-8/interface
-  (open scheme)
-  (files srfi-8))
-
 (define-structure let-opt-expanders let-opt-expanders/interface
-  (open scheme
-	signals
-	srfi-8)
+  (open scheme (subset signals (error warn)) srfi-8)
   (files let-opt-expanders))
 
 (define-structure let-opt let-opt/interface
-  (open scheme signals)
+  (open scheme (subset signals (error warn)) receiving)
   (for-syntax (open scheme let-opt-expanders))
   (files let-opt))
 
 (define-structure srfi-89 srfi-89/interface
-  (open scheme srfi-1 let-opt)
-  (for-syntax (open scheme let-opt (subset signals (syntax-error)) srfi-1))
+  (open scheme let-opt)
+  (for-syntax (open scheme let-opt
+                    (subset signals (syntax-error))
+                    (subset srfi-1 (every take-while drop-while))))
   (files srfi-89))
-
-(define-structure srfi-78 srfi-78/interface
-  (open scheme srfi-23 srfi-42 pp)
-  (files srfi-78)
-  (begin (define check:write p)))
-
-(define-structure srfi-39 srfi-39/interface
-  (open scheme)
-  (files srfi-39))
 
 (define-structures
     ((pantene:condition-definition pantene:condition-definition/interface)
      (pantene:condition-type       pantene:condition-type/interface)
      (pantene:restarter           pantene:restarter/interface))
   (open scheme
-        (modify signals (rename (error system:error)))
-        srfi-1 srfi-6 srfi-9 srfi-13 srfi-14 srfi-39 srfi-89 signals)
+        (modify signals  (expose system:error) (rename (error system:error)))
+        (subset srfi-1 (cons* every any find find-tail))
+        (subset srfi-13 (string-null? string-prefix-ci?))
+        srfi-6 srfi-9 srfi-14 srfi-39 srfi-89)
   (files condition-type
          taxonomy
          condition
@@ -56,7 +45,7 @@
   (files (tests test-condition-types)))
 
 (define-structure pantene:restarter-tests (export)
-  (open scheme pantene srfi-1 srfi-78)
+  (open scheme pantene srfi-78)
   (files (tests test-restarters)))
 
 (define-structure pantene:tests (export)
